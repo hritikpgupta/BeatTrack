63aee77c7f49e6565d448c182216569751f7fc01
hritikpgupta <hritikgupta722@gmail.com>
2024-05-03 14:56:52 +0100

    Added Build Logic module

---
+++ build-logic/convention/src/main/java/com/caletasolutions/convention/MyClass.kt
@@ -0,0 +1,4 @@
+package com.caletasolutions.convention
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             content {
@@ -19,9 +20,8 @@ dependencyResolutionManagement {
     }
 }
 
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 rootProject.name = "BeatTrack"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
 include(":auth:data")
 include(":auth:domain")
