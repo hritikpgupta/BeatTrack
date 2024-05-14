4c254e65244d247179ad0239e6016b4177d0dc0e
Hritik Gupta <36474421+hritikpgupta@users.noreply.github.com>
2024-05-14 21:54:11 +0100

    Set up permission handling

--- run/presentation/src/main/java/com/hg/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/hg/run/presentation/active_run/ActiveRunScreen.kt
@@ -2,7 +2,13 @@
 
 package com.hg.run.presentation.active_run
 
-
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,24 +17,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hg.core.presentation.designsystem.BeatTrackTheme
 import com.hg.core.presentation.designsystem.StartIcon
 import com.hg.core.presentation.designsystem.StopIcon
+import com.hg.core.presentation.designsystem.components.BeatTrackDialog
 import com.hg.core.presentation.designsystem.components.BeatTrackFloatingActionButton
+import com.hg.core.presentation.designsystem.components.BeatTrackOutLinedActionButton
 import com.hg.core.presentation.designsystem.components.BeatTrackScaffold
 import com.hg.core.presentation.designsystem.components.BeatTrackToolbar
 import com.hg.run.presentation.R
-import com.hg.core.presentation.designsystem.BeatTrackTheme
-import com.hg.core.presentation.designsystem.StartIcon
-import com.hg.core.presentation.designsystem.StopIcon
-import com.hg.core.presentation.designsystem.components.BeatTrackFloatingActionButton
-import com.hg.core.presentation.designsystem.components.BeatTrackScaffold
-import com.hg.core.presentation.designsystem.components.BeatTrackToolbar
 import com.hg.run.presentation.active_run.components.RunDataCard
+import com.hg.run.presentation.util.hasLocationPermission
+import com.hg.run.presentation.util.hasNotificationPermission
+import com.hg.run.presentation.util.shouldShowLocationPermissionRationale
+import com.hg.run.presentation.util.shouldShowNotificationPermissionRationale
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -46,6 +54,57 @@ private fun ActiveRunScreen(
     state: ActiveRunState,
     onAction: (ActiveRunAction) -> Unit
 ) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { perms ->
+        val hasCourseLocationPermission = perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        val hasFineLocationPermission = perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+        val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+            perms[Manifest.permission.POST_NOTIFICATIONS] == true
+        } else true
+
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = hasCourseLocationPermission && hasFineLocationPermission,
+                showLocationRationale = showLocationRationale
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = hasNotificationPermission,
+                showNotificationPermissionRationale = showNotificationRationale
+            )
+        )
+    }
+
+    LaunchedEffect(key1 = true) {
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = context.hasLocationPermission(),
+                showLocationRationale = showLocationRationale
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = context.hasNotificationPermission(),
+                showNotificationPermissionRationale = showNotificationRationale
+            )
+        )
+
+        if (!showLocationRationale && !showNotificationRationale) {
+            permissionLauncher.requestBeatTrackPermissions(context)
+        }
+    }
+
     BeatTrackScaffold(
         withGradient = false,
         topAppBar = {
@@ -68,7 +127,7 @@ private fun ActiveRunScreen(
                     onAction(ActiveRunAction.OnToggleRunClick)
                 },
                 iconSize = 20.dp,
-                contentDescription = if(state.shouldTrack) {
+                contentDescription = if (state.shouldTrack) {
                     stringResource(id = R.string.pause_run)
                 } else {
                     stringResource(id = R.string.start_run)
@@ -91,7 +150,62 @@ private fun ActiveRunScreen(
             )
         }
     }
+    if (state.showLocationRationale || state.showNotificationRationale) {
+        BeatTrackDialog(
+            title = stringResource(id = R.string.permission_required),
+            onDismiss = { /* Normal dismissing not allowed for permissions */ },
+            description = when {
+                state.showLocationRationale && state.showNotificationRationale -> {
+                    stringResource(id = R.string.location_notification_rationale)
+                }
+                state.showLocationRationale -> {
+                    stringResource(id = R.string.location_rationale)
+                }
+                else -> {
+                    stringResource(id = R.string.notification_rationale)
+                }
+            },
+            primaryButton = {
+                BeatTrackOutLinedActionButton(
+                    text = stringResource(id = R.string.okay),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.DismissRationaleDialog)
+                        permissionLauncher.requestBeatTrackPermissions(context)
+                    }
+                )
+            }
+        )
+    }
 }
+
+
+private fun ActivityResultLauncher<Array<String>>.requestBeatTrackPermissions(
+    context: Context
+) {
+    val hasLocationPermission = context.hasLocationPermission()
+    val hasNotificationPermission = context.hasNotificationPermission()
+
+    val locationPermissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+    )
+    val notificationPermission = if(Build.VERSION.SDK_INT >= 33) {
+        arrayOf(Manifest.permission.POST_NOTIFICATIONS)
+    } else arrayOf()
+
+    when {
+        !hasLocationPermission && !hasNotificationPermission -> {
+            launch(locationPermissions + notificationPermission)
+        }
+        !hasLocationPermission -> launch(locationPermissions)
+        !hasNotificationPermission -> launch(notificationPermission)
+    }
+}
+
+
+
+
 
 @Preview
 @Composable
