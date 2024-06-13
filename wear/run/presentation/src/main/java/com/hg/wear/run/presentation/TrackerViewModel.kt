20c25acde26fca367e9854a9656989bd861175b5
hritikpgupta <hritikgupta722@gmail.com>
2024-06-13 12:10:38 +0100

    Setting wear os permission

--- wear/run/presentation/src/main/java/com/hg/wear/run/presentation/TrackerViewModel.kt
+++ wear/run/presentation/src/main/java/com/hg/wear/run/presentation/TrackerViewModel.kt
@@ -4,13 +4,59 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hg.wear.run.domain.ExerciseTracker
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
-class TrackerViewModel: ViewModel() {
+class TrackerViewModel(
+    private val exerciseTracker: ExerciseTracker
+) : ViewModel() {
 
-    var state by mutableStateOf(TrackerState())
+    var state by mutableStateOf(TrackerState(
+        isConnectedPhoneNearby = true
+    ))
         private set
 
-    fun onAction(action: TrackerAction) {
+    private val hasBodySensorPermission = MutableStateFlow(false)
 
+    init {
+        viewModelScope.launch {
+            hasBodySensorPermission.flatMapLatest { isGranted ->
+                if (isGranted) {
+                    exerciseTracker.heartRate
+                } else flowOf()
+            }.onEach {
+                state = state.copy(
+                    heartRate = it
+                )
+            }.launchIn(this)
+        }
+    }
+
+    fun onAction(action: TrackerAction) {
+        when (action) {
+            is TrackerAction.OnBodySensorPermissionResult -> {
+                hasBodySensorPermission.value = action.isGranted
+                if (action.isGranted) {
+                    viewModelScope.launch {
+                        val isHeartRateTrackingSupported =
+                            exerciseTracker.isHeartRateTrackingSupported()
+                        state = state.copy(
+                            canTrackHeartRate = isHeartRateTrackingSupported
+                        )
+                        exerciseTracker.prepareExercise()
+                        exerciseTracker.startExercise()
+                    }
+                }
+            }
+
+            TrackerAction.OnFinishRunClick -> Unit
+            TrackerAction.OnToggleRunClick -> Unit
+        }
     }
 }
